ff8ce476ffd3748a31887be3f13f9c788f90fd8e
Harper <harperliulove@gmail.com>
2019-12-07 22:52:59 +1100

    finishing

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # MonashBootcampWeek7
 Monash Bootcamp for Week 7
+
+This is an application that can be used to generate a pdf based on the 
+user's github name and color-selection
+
+Instruction:
+1. run node.js
+2. enter the user's github username
+3. select the color style that you'd like to have
+4. open the Github_Profile.pdf to see the magic!
+
+About the color-selection:
+The reason why I chooese not to let the user input their favourite color
+is that random color may make the pdf displayed in a not so great way
+(they might choose some color such as red or gray that are not so good to see in resume)
+Therefore, I pre-defined some color themes for user to choose from, which would make the pdf
+display in a more user-friendly way.
+
+Further Improvement:
+In the card display, the number could not made to be centered, even though in the HTML, they are centered.
+However, no matter what I do to change the style, they are still alighed to left in pdf.
+Still working on it.
--- index.js
+++ index.js
@@ -8,20 +8,53 @@ inquirer
     .prompt([
             {
                 message: "Enter your GitHub username:",
-                name: "username"
+                name: "username",
+                type: "input"
             },
             {
                 message: "What's your favourite color?",
-                name: "color"
+                name: "color",
+                type: 'rawlist',
+                choices: [
+                    "olive green", "light green", "sky blue", "yellow", 
+                    "red", "light gray", "white", "black"
+                ]
             }
         ])
     .then(function({ username,color }) {
+        console.log(color);
         var gh = new GitHub({
             username: "harper774",
             password: "lyy999774LYY"
         });
-        
-        var ghUser = gh.getUser(username); 
+        switch (color){
+            case "olive green":
+                color = "primary";
+                break;
+            case "light green":
+                color = "success";
+                break;
+            case "sky blue":
+                color = "info";
+                break;
+            case "yellow":
+                color = "warning";
+                break;
+            case "red":
+                color = "danger";
+                break;
+            case "light gray":
+                color = "light";
+                break;
+            case "white":
+                color = "white";
+                break;
+            case "black":
+                color = "dark";
+                break;
+        }
+        console.log(color);
+        const ghUser = gh.getUser(username); 
         //getProfile can return the username, the number of followers,
         //the number of followings, and the company's name,
         //the location, the link to github page and blog
@@ -33,125 +66,189 @@ inquirer
                 if(err) console.log(err);
                 HTMLContent = `
                 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" integrity="sha384-KA6wR/X5RY4zFAHpv/CnoG2UW1uogYfdnP67Uv7eULvTveboZJg0qUpmJZb5VqzN" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
-    <title>This is Git Hub User Page</title>
-</head>
-<body>
-    <section class="hero is-medium is-dark is-bold">
-        <div class="hero-body" style="padding: 50px">
-            <div class="container has-text-centered">
-                <h1 class="title">
-                    <figure class="image is-128x128" style="margin:auto">
-                        <img class="is-rounded" src=${profile.avatar_url}>
-                    </figure>
-                    <br>
-                    This is ${profile.name}'s Github Profile
-                    <br><br>
-                </h1>
-                <h2 class="subtitle">
-                    <i class="fas fa-building"></i>
-                    Currently @ ${profile.company} 
-                    <br><br>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" integrity="sha384-KA6wR/X5RY4zFAHpv/CnoG2UW1uogYfdnP67Uv7eULvTveboZJg0qUpmJZb5VqzN" crossorigin="anonymous">
+                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+                    <title>This is Git Hub User Page</title>
 
-                    <i class="fas fa-location-arrow"></i> 
-                    <a href=${profile.location}>
-                        ${profile.location}
-                    </a>
-                    &nbsp;
+                    <style>
+                        .card {
+                            background-color: ${color};
+                        }
+                        //this could not be centered--still finding solution
+                        .panel-block {
+                            justify-content: center;
+                        }
+                    </style>
+                </head>
+                <body>
+                    <section class="hero is-medium is-bold is-`+color+`">
+                        <div class="hero-body" style="padding: 50px">
+                            <div class="container has-text-centered">
+                                <h1 class="title">
+                                    <figure class="image is-128x128" style="margin:auto">
+                                        <img class="is-rounded" src=${profile.avatar_url}>
+                                    </figure>
+                                    <br>
+                                    This is ${profile.name}'s Github Profile
+                                    <br><br>
+                                </h1>
+                                <h2 class="subtitle">
+                                    <i class="fas fa-building"></i>
+                                    Currently @ ${profile.company} 
+                                    <br><br>
 
-                    <i class="fab fa-github"></i>
-                    <a href=${profile.html_url}>
-                        Github
-                    </a>
-                    &nbsp;
-                    <i class="fas fa-blog"></i>
-                    <a href=${profile.blog}>
-                        Blog
-                    </a>
-                </h2>
-            </div>
-        </div>
-    </section>
+                                    <i class="fas fa-location-arrow"></i> 
+                                    <a href=${profile.location}>
+                                        ${profile.location}
+                                    </a>
+                                    &nbsp;
 
-    <section class="section" id="indexPage">
-        <div class="container">
-            <div class="columns">
-                <div class="column is-half is-offset-one-quarter">
-                    <div class="card">
-                        <header class="card-header">
-                            <p class="card-header-title is-centered">
-                            Public Repositories
-                            </p>
-                        </header>
-                        <div class="card-content has-text-centered">
-                            <div class="content">
-                                ${profile.public_repos}
+                                    <i class="fab fa-github"></i>
+                                    <a href=${profile.html_url}>
+                                        Github
+                                    </a>
+                                    &nbsp;
+                                    <i class="fas fa-blog"></i>
+                                    <a href=${profile.blog}>
+                                        Blog
+                                    </a>
+                                    <br><br>
+                                    <h>${profile.bio}</h>
+                                </h2>
                             </div>
                         </div>
-                    </div>
-                </div>
-            </div>
+                    </section>
 
-            <div class="columns">
-                <div class="column is-half is-offset-one-quarter">
-                    <div class="card">
-                        <header class="card-header">
-                            <p class="card-header-title is-centered">
-                                GitHub Stars
-                            </p>
-                        </header>
-                        <div class="card-content has-text-centered">
-                            <div class="content">
-                                ${repos.length}
+                    <section class="section" id="indexPage">
+                        <div class="container">
+                            <div class="columns">
+                                <div class="column is-half is-offset-one-quarter">
+                                    <article class="panel is-`+color+`">
+                                        <p class="panel-heading has-text-centered">
+                                                Public Repositories
+                                        </p>
+                                        <div class="panel-block">
+                                            <a class="panel-block is-active has-text-centered">
+                                                <span class="panel-icon">
+                                                    <i class="fas fa-book" aria-hidden="true"></i>
+                                                </span>
+                                                ${profile.public_repos}
+                                            </a>
+                                        </div>                          
+                                    </article>
+                                    <!-- <div class="card">
+                                        <header class="card-header">
+                                            <p class="card-header-title has-text-centered">
+                                            Public Repositories
+                                            </p>
+                                        </header>
+                                        <div class="card-content has-text-centered">
+                                            <div class="content">
+                                                ${profile.public_repos}
+                                            </div>
+                                        </div>
+                                    </div> -->
+                                </div>
+                            </div>
+
+                            <div class="columns">
+                                <div class="column is-half is-offset-one-quarter">
+                                    <article class="panel is-`+color+`">
+                                        <p class="panel-heading has-text-centered">
+                                                Github Stars
+                                        </p>
+                                        <div class="panel-block">
+                                            <a class="panel-block is-active has-text-centered">
+                                                <span class="panel-icon">
+                                                    <i class="fas fa-book" aria-hidden="true"></i>
+                                                </span>
+                                                ${repos.length}
+                                            </a>
+                                        </div>                          
+                                    </article>
+                                    <!-- <div class="card">
+                                        <header class="card-header">
+                                            <p class="card-header-title has-text-centered">
+                                                GitHub Stars
+                                            </p>
+                                        </header>
+                                        <div class="card-content has-text-centered">
+                                            <div class="content">
+                                                ${repos.length}
+                                            </div>
+                                        </div>
+                                    </div> -->
+                                </div>
+                            </div>
+
+                            <div class="columns">
+                                <div class="column is-half is-offset-one-quarter">
+                                    <article class="panel is-`+color+`">
+                                        <p class="panel-heading has-text-centered">
+                                                Followers
+                                        </p>
+                                        <div class="panel-block">
+                                            <a class="panel-block is-active has-text-centered">
+                                                <span class="panel-icon">
+                                                    <i class="fas fa-book" aria-hidden="true"></i>
+                                                </span>
+                                                ${profile.followers}
+                                            </a>
+                                        </div>                          
+                                    </article>
+                                    <!-- <div class="card">
+                                        <header class="card-header">
+                                            <p class="card-header-title has-text-centered">
+                                                Followers
+                                            </p>
+                                        </header>
+                                        <div class="card-content has-text-centered">
+                                            <div class="content">
+                                                ${profile.followers}
+                                            </div>
+                                        </div>
+                                    </div> -->
+                                </div>
+                            </div>
+
+                            <div class="columns">
+                                <div class="column is-half is-offset-one-quarter">
+                                    <article class="panel is-`+color+`">
+                                        <p class="panel-heading has-text-centered">
+                                                Following
+                                        </p>
+                                        <div class="panel-block">
+                                            <a class="panel-block is-active has-text-centered">
+                                                <span class="panel-icon">
+                                                    <i class="fas fa-book" aria-hidden="true"></i>
+                                                </span>
+                                                ${profile.following}
+                                            </a>
+                                        </div>                          
+                                    </article>
+                                    <!-- <div class="card">
+                                        <header class="card-header">
+                                            <p class="card-header-title has-text-centered">
+                                                Following
+                                            </p>
+                                        </header>
+                                        <div class="card-content has-text-centered">
+                                            <div class="content">
+                                                ${profile.following}
+                                            </div>
+                                        </div>
+                                    </div> -->
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="columns">
-                <div class="column is-half is-offset-one-quarter">
-                    <div class="card">
-                        <header class="card-header">
-                            <p class="card-header-title is-centered">
-                                Followers
-                            </p>
-                        </header>
-                        <div class="card-content has-text-centered">
-                            <div class="content">
-                                ${profile.followers}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="columns">
-                <div class="column is-half is-offset-one-quarter">
-                    <div class="card">
-                        <header class="card-header">
-                            <p class="card-header-title is-centered">
-                                Following
-                            </p>
-                        </header>
-                        <div class="card-content has-text-centered">
-                            <div class="content">
-                                ${profile.following}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-</body>
-</html>`;
+                    </section>
+                </body>
+                </html>`;
                 fs.writeFileSync("index.html", HTMLContent, (err) => {
                     if(err) console.log(err);
                     console.log("Saved to index.html!");
@@ -163,26 +260,15 @@ inquirer
                     border: "10mm"
                 };
                 const users = [
-                    // {
-                    //     name:"Shyam",
-                    //     age:"26"
-                    // },
-                    // {
-                    //     name:"Navjot",
-                    //     age:"26"
-                    // },
-                    // {
-                    //     name:"Vitthal",
-                    //     age:"26"
-                    // }
                 ]
-                
                 const document = {
                     html: html,
+                    //although there is nothing in the users,
+                    //but deleting it will incur errors
                     data: {
                         users: users
                     },
-                    path: "./output.pdf"
+                    path: "./Github_Profile.pdf"
                 };
 
                 pdf.create(document, options)
